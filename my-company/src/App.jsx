f6cac3d731f930e28802a432737c67dfd1a77dff
sultanmawdood <sultanmawdoodfawzi@gmail.com>
2025-11-08 19:09:51 +0200

    Your commit message

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,3 +1,13 @@
+import UserProfile from './components/UserProfile';
+
+import Header from './components/Header';
+
+import MainContent from './components/MainContent';
+
+import Footer from './components/Footer';
+
+import WelcomeMessage from './components/WelcomeMessage';
+
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -6,8 +16,17 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
-  return (
+  return ( 
     <>
+    <WelcomeMessage />
+    <div>
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>
+    <div>
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+    </div>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -21,6 +40,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
@@ -30,6 +50,7 @@ function App() {
       </p>
     </>
   )
+  
 }
 
 export default App
