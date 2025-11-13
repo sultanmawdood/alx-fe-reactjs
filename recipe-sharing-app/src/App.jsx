41a108df6728c88224e67c05a7800f7d49d275c2
sultanmawdood <sultanmawdoodfawzi@gmail.com>
2025-11-13 19:46:50 +0200

    recipe store

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import AddRecipeForm from "./components/AddRecipeForm"
+import RecipeList from "./components/RecipeList"
+
+function App() {
+
+  return (
+    <>
+    <RecipeList/>
+    <AddRecipeForm/>
+    </>
+  )
+}
+
+export default App
