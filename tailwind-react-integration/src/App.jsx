35a9833976c37ddf10bf506ed2c52ee5a6aec766
sultanmawdood <sultanmawdoodfawzi@gmail.com>
2025-11-30 12:35:50 +0200

    added

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,20 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
-        <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+        <h1 class="text-3xl font-bold underline">Hello world!</h1>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
