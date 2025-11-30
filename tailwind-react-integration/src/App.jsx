c02926dff3aa70b75a5e5776b957893a5c7bcfa1
sultanmawdood <sultanmawdoodfawzi@gmail.com>
2025-11-30 11:50:56 +0200

    Save local changes before pull

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,13 +1,20 @@
-import { useState } from "react";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
     <>
       <div>
-        <h1 class="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className='text-3xl font-bold underline'>
+        Hello Sultan Welcome to react js
+      </h1>  
       </div>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
