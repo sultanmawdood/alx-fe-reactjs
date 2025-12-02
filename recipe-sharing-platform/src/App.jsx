e3bf23d30054bff4bdefeb1eb156a1a626de9f35
sultanmawdood <sultanmawdoodfawzi@gmail.com>
2025-12-02 17:09:01 +0200

    Update App.jsx

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,14 +1,17 @@
-import { useState } from "react";
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import RecipeDetail from "./pages/RecipeDetail";
 
-function App() {
-  const [count, setCount] = useState(0);
 
+export default function App() {
   return (
-    <>
-      <div></div>
-    </>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/recipe/:id" element={<RecipeDetail />} />
+        <Route path="/search" element={<SearchPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default App;
