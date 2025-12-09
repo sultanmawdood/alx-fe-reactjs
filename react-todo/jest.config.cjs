71e3d46ab9292dc5358817ff150d310e9046e92c
sultanmawdood <sultanmawdoodfawzi@gmail.com>
2025-12-09 11:52:20 +0200

    Delete react-todo directory

--- react-todo/jest.config.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  testEnvironment: 'jsdom',
-  transform: {
-    '^.+\\.jsx?$': 'babel-jest',
-  },
-  moduleNameMapper: {
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-  },
-};
